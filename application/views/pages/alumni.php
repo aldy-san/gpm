86868594e1358e12f7cd3917b4ae8e081cd33256
nursmdev <nsmaulidi12@gmail.com>
2022-08-10 10:32:19 +0700

    adjust login mitra alumnit

--- application/views/pages/alumni.php
+++ application/views/pages/alumni.php
@@ -1,8 +1,8 @@
-<form class="row justify-content-center align-items-center auth-form">
+<form class="d-flex justify-content-center align-items-center auth-form">
     <div class="col-11 col-md-8 col-lg-6 rounded-2 border p-4 bg-white">
         <h4 class="text-center mb-4">Identitas Alumni</h4>
         <div class="row">
-            <div class="mb-3 col-6">
+            <div class="mb-3 col-12 col-md-6">
                 <label for="name" class="form-label">Nama</label>
                 <input type="text" class="form-control <?= form_error('name') ? 'is-invalid': ''; ?>" name="name"
                     id="name" placeholder="nama" autocomplete="off" value="<?= set_value('name'); ?>">
@@ -10,61 +10,57 @@
                     <?= form_error('name'); ?>
                 </div>
             </div>
-            <div class="mb-3 col-6">
+            <div class="mb-3 col-12 col-md-6">
                 <label for="email" class="form-label">Email</label>
                 <input type="text" class="form-control <?= form_error('email') ? 'is-invalid': ''; ?>" name="email"
-                    id="email" placeholder="nama@example.com" autocomplete="off" value="<?= set_value('email'); ?>">
+                    id="email" placeholder="nama@gmail.com" autocomplete="off" value="<?= set_value('email'); ?>">
                 <div class="invalid-feedback">
                     <?= form_error('email'); ?>
                 </div>
             </div>
-            <div class="form-group mb-3 col-6">
-                <label for="chart" class="mb-2">Program Studi</label>
+            <div class="form-group mb-3 col-12 col-md-6">
+                <label for="prodi" class="mb-2">Program Studi</label>
                 <fieldset>
-                    <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
+                    <select class="form-select" name="prodi">
+                        <option value=""> D3 Elektronika </option>
+                        <option value=""> D3 Elektro</option>
+                        <option value=""> S1 Teknik Informatika</option>
+                        <option value=""> S1 Teknik Elektro</option>
+                        <option value=""> S1 Pendidikan Teknik Informatika</option>
+                        <option value=""> S1 Pendidikan Teknik Elektro</option>
+                        <option value=""> S2 Teknik Elektro</option>
                     </select>
                 </fieldset>
             </div>
-            <div class="form-group mb-3 col-6">
+            <div class="form-group mb-3 col-12 col-md-6">
                 <label for="chart" class="mb-2">Aktifitas Setelah Lulus</label>
                 <fieldset>
                     <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
+                        <option value=""> Bekerja </option>
+                        <option value=""> Wirausaha </option>
+                        <option value=""> Studi Lanjut </option>
+                        <option value=""> Belum Ketiganya </option>
+
                     </select>
                 </fieldset>
             </div>
-            <div class="form-group mb-3 col-6">
-                <label for="chart" class="mb-2">Tahun Masuk</label>
-                <fieldset>
-                    <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
-                    </select>
-                </fieldset>
+            <div class="form-group mb-3 col-12 col-md-6">
+                <label for="" class="mb-2">Tahun Masuk</label>
+                <select class="form-select" aria-label="Default select example" onfocus="this.size=4;"
+                    onblur="this.size=1;" onchange="this.size=1; this.blur();">
+                    <?php for ($year = (int)date('Y'); 1900 <= $year; $year--): ?>
+                    <option value=" <?=$year;?>"><?=$year;?></option>
+                    <?php endfor; ?>
+                </select>
             </div>
-            <div class="form-group mb-3 col-6">
-                <label for="chart" class="mb-2">Tahun Lulus</label>
-                <fieldset>
-                    <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
-                    </select>
-                </fieldset>
+            <div class="form-group mb-3 col-12 col-md-6">
+                <label for="" class="mb-2">Tahun Lulus</label>
+                <select class="form-select" aria-label="Default select example" onfocus="this.size=4;"
+                    onblur="this.size=1;" onchange="this.size=1; this.blur();">
+                    <?php for ($year = (int)date('Y'); 1900 <= $year; $year--): ?>
+                    <option value=" <?=$year;?>"><?=$year;?></option>
+                    <?php endfor; ?>
+                </select>
             </div>
         </div>
         <button class="btn btn-primary">Mulai Isi Angket</button>
--- application/views/pages/mitra.php
+++ application/views/pages/mitra.php
@@ -1,16 +1,16 @@
-<form class="row justify-content-center align-items-center auth-form">
+<form class="d-flex justify-content-center align-items-center auth-form">
     <div class="col-11 col-md-8 col-lg-6 rounded-2 border p-4 bg-white">
         <h4 class="text-center mb-4">Identitas Mitra</h4>
         <div class="row">
-            <div class="mb-3 col-6">
-                <label for="name" class="form-label">Jabatan Pengisi</label>
-                <input type="text" class="form-control <?= form_error('name') ? 'is-invalid': ''; ?>" name="name"
-                    id="name" placeholder="nama" autocomplete="off" value="<?= set_value('name'); ?>">
+            <div class="mb-3 col-12 col-md-6">
+                <label for="jabatan" class="form-label">Jabatan Pengisi</label>
+                <input type="text" class="form-control <?= form_error('jabatan') ? 'is-invalid': ''; ?>" name="jabatan"
+                    id="jabatan" placeholder="Jabatan" autocomplete="off" value="<?= set_value('jabatan'); ?>">
                 <div class="invalid-feedback">
-                    <?= form_error('name'); ?>
+                    <?= form_error('jabatan'); ?>
                 </div>
             </div>
-            <div class="mb-3 col-6">
+            <div class="mb-3 col-12 col-md-6">
                 <label for="name" class="form-label">Nama Instansi</label>
                 <input type="text" class="form-control <?= form_error('name') ? 'is-invalid': ''; ?>" name="name"
                     id="name" placeholder="nama" autocomplete="off" value="<?= set_value('name'); ?>">
@@ -18,49 +18,44 @@
                     <?= form_error('name'); ?>
                 </div>
             </div>
-            <div class="mb-3 col-6">
-                <label for="name" class="form-label">Nomor Telepon</label>
-                <input type="text" class="form-control <?= form_error('name') ? 'is-invalid': ''; ?>" name="name"
-                    id="name" placeholder="nama" autocomplete="off" value="<?= set_value('name'); ?>">
+            <div class="mb-3 col-12 col-md-6">
+                <label for="telepon" class="form-label">Nomor Telepon</label>
+                <input type="text" class="form-control <?= form_error('telepon') ? 'is-invalid': ''; ?>" name="telepon"
+                    id="telepon" placeholder="08xxxxxxxxxxx" autocomplete="off" value="<?= set_value('telepon'); ?>">
                 <div class="invalid-feedback">
-                    <?= form_error('name'); ?>
+                    <?= form_error('telepon'); ?>
                 </div>
             </div>
-            <div class="form-group mb-3 col-6">
-                <label for="chart" class="mb-2">Tahun Berdiri</label>
-                <fieldset>
-                    <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
-                    </select>
-                </fieldset>
-            </div>
-            <div class="form-group mb-3 col-6">
+            <div class="form-group mb-3 col-12 col-md-6">
                 <label for="chart" class="mb-2">Skala Operasional</label>
                 <fieldset>
                     <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
+                        <option value="">Internasional</option>
+                        <option value="">Nasional</option>
+                        <option value="">Swasta</option>
+                        <option value="">BUMN</option>
+                        <option value="">Negeri</option>
+                        <option value="">Milik Sendiri</option>
                     </select>
                 </fieldset>
             </div>
-            <div class="form-group mb-3 col-6">
-                <label for="chart" class="mb-2">Tahun Kerjasama</label>
-                <fieldset>
-                    <select class="form-select" name="chart">
-                        <option value="bar">
-                            Bar
-                        </option>
-                        <option value="pie">
-                            Pie</option>
-                    </select>
-                </fieldset>
+            <div class="form-group mb-3 col-12 col-md-6">
+                <label for="" class="mb-2">Tahun Berdiri</label>
+                <select class="form-select" aria-label="Default select example" onfocus="this.size=4;"
+                    onblur="this.size=1;" onchange="this.size=1; this.blur();">
+                    <?php for ($year = (int)date('Y'); 1900 <= $year; $year--): ?>
+                    <option value=" <?=$year;?>"><?=$year;?></option>
+                    <?php endfor; ?>
+                </select>
+            </div>
+            <div class="form-group mb-3 col-12 col-md-6">
+                <label for="" class="mb-2">Tahun Kerjasama</label>
+                <select class="form-select" aria-label="Default select example" onfocus="this.size=4;"
+                    onblur="this.size=1;" onchange="this.size=1; this.blur();">
+                    <?php for ($year = (int)date('Y'); 1900 <= $year; $year--): ?>
+                    <option value=" <?=$year;?>"><?=$year;?></option>
+                    <?php endfor; ?>
+                </select>
             </div>
         </div>
         <button class="btn btn-primary">Mulai Isi Angket</button>
