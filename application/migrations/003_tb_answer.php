c2219ee53331122e27b2afc13ab96db25744581a
nursmdev <nsmaulidi12@gmail.com>
2022-08-12 09:03:18 +0700

    add seed

--- application/migrations/003_tb_answer.php
+++ application/migrations/003_tb_answer.php
@@ -8,7 +8,7 @@ class Migration_Tb_answer extends CI_Migration{
     }
     public function up()
     {
-        $this->dbforge->drop_table('answer');
+        //$this->dbforge->drop_table('answer');
         $this->dbforge->add_field(array(
             'id' => array(
                 'type' => 'INT',
@@ -35,6 +35,78 @@ class Migration_Tb_answer extends CI_Migration{
         ));
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('answer');
+        $seed = [
+            [
+                'id_user' => 1,
+                'id_survei' => 3,
+                'answer' => 'cool',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 2,
+                'id_survei' => 3,
+                'answer' => 'yes',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 3,
+                'id_survei' => 3,
+                'answer' => 'yo',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 4,
+                'id_survei' => 3,
+                'answer' => 'yes',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 1,
+                'id_survei' => 9,
+                'answer' => 'yo',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 1,
+                'id_survei' => 9,
+                'answer' => 'ma',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 1,
+                'id_survei' => 10,
+                'answer' => '81-100',
+                'detail' => '87'],
+            [
+                'id_user' => 2,
+                'id_survei' => 10,
+                'answer' => '61-80',
+                'detail' => '66'],
+            [
+                'id_user' => 2,
+                'id_survei' => 2,
+                'answer' => 'asdasd',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 3,
+                'id_survei' => 2,
+                'answer' => 'sadasd',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 3,
+                'id_survei' => 2,
+                'answer' => 'asdasd',
+                'detail' => ''
+            ],
+            [
+                'id_user' => 4,
+                'id_survei' => 2,
+                'answer' => 'sadasd',
+                'detail' => '']
+        ];
+        $this->db->insert_batch('answer', $seed); 
     }
     public function down()
     {
